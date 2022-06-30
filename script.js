cf838666931ef55dd241e9413a6a0d7f6ae96e3b
jstoozy23 <jstoozy23@gmail.com>
2022-06-30 12:19:31 -0700

    Attempting to add player api pull

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ var ppg = document.getElementById("pointsPerGame");
 var rpg = document.getElementById("reboundsPerGame");
 var apg = document.getElementById("assistsPerGame");
 var team = 'Phoenix Suns';
-var player = "booker";
+var player = "Booker";
 var teamSearchurl = 'https://api-nba-v1.p.rapidapi.com/teams?name=' + team;
+var playerSearchurl = "https://api-nba-v1.p.rapidapi.com/players?&team=28&season=2021&id=" + player;
 const options = {
     method: 'GET',
     headers: {
@@ -15,12 +16,27 @@ fetch(teamSearchurl, options)
     .then(response => response.json())
     .then(response => {
     console.log("team",response)
-    var url = 'https://api-nba-v1.p.rapidapi.com/players/statistics?team=' + response.response[0].id + '&season=2020';
+
+  
+fetch(player, options)
+    .then(response => response.json())
+    .then(response => {
+        console.log("Players",response)
+
+    
+    var url = 'https://api-nba-v1.p.rapidapi.com/players/statistics?season=2021&team=' + response.response[0].id + //response.response[0].id;
   
   fetch(url, options)
     .then(response => response.json())
     .then(response => {
-        console.log("gamesbyteam",response)
+        console.log("allGamesofAllPlayers",response)
+
+
+  
+
+
+
+
   //       var url2 = 'https://api-nba-v1.p.rapidapi.com/players/statistics?team=' + response.response[0].id + "&" + player + '&season=2020';
 
   // fetch(url2, options)
@@ -40,7 +56,7 @@ fetch(teamSearchurl, options)
         console.log(averageAstPerGame);
         apg.textContent = averageAstPerGame
   // }) 
-  })})
+  })})})
 
 function avgPoint(array){
     let sum = array.reduce(function (cumulativePoints,thisGame) {
